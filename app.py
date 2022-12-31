c09dfd70b142118390376cafaddb43db931fde05
93k00c3 <placek.shoot@gmail.com>
2022-12-31 10:42:50 +0100

    Tworzenie osobnych folderów dla każdego z uploadów, implementacja funkcji zbierającej wszystkie pliki z folderu

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
+import uuid
+
 from flask import Flask, flash, request, redirect, render_template, url_for
 from flask_wtf import FlaskForm
 from werkzeug.utils import secure_filename
 from wtforms import FileField, SubmitField
-import os
+
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '.'
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+parent_path = app.config['UPLOAD_FOLDER']
 SECRET_KEY = os.urandom(32)
 ALLOWED_EXTENSIONS = {'flac', 'alac', 'mp3', 'wav'}
 app.config['SECRET_KEY'] = SECRET_KEY
@@ -17,6 +21,41 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+FFMPEG = ('ffmpeg -hide_banner -loglevel {loglevel}'
+          ' -i "{source}" -f mp3 -ab {bitrate} -vcodec copy "{target}"')
+
+# def parse_args():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument(
+#         '-r', '--recursive', default=False, action='store_true',
+#         help='search subdirectories')
+#     parser.add_argument(
+#         '-b', '--bitrate', default='320k', metavar='BITRATE',
+#         choices=('256k', '320k'),
+#         help='desired bitrate of the target files (default 320k)')
+#     parser.add_argument(
+#         '--ffmpeg-loglevel', default='error', metavar='LEVEL',
+#         choices=('debug', 'verbose', 'info', 'warning',
+#                  'error', 'fatal', 'panic', 'quiet'),
+#         help='desired level of ffmpeg output (default error)')
+#     parser.add_argument(
+#         'sources', nargs='+', metavar='SOURCE',
+#         help='file or directory to convert')
+#     parser.add_argument(
+#         'target', nargs=1, metavar='TARGET',
+#         help='target directory for the converted files and/or directories')
+#     args = parser.parse_args()
+#     args.sources = [Path(src) for src in args.sources]
+#     args.target = Path(args.target[0])
+#     return args
+#
+# def call_ffmpeg(source, target, bitrate='320k', loglevel='error'):
+#     ffmpeg = FFMPEG.format(loglevel=loglevel, source=source,
+#                            bitrate=bitrate, target=target)
+#     return subprocess.check_output(
+#         ffmpeg, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+
+
 @app.route('/', methods =["GET", "POST"])
 def upload_file():
     if request.method == 'POST':
@@ -27,12 +66,24 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            path = os.path.join(parent_path + filename.split('.')[0])
+            os.mkdir(path)
+            file.save(os.path.join(parent_path +(filename.split('.')[0]), filename))
             flash('File successfully uploaded')
             return redirect(url_for('upload_file', name=filename))
         else:
             flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
     return render_template('index.html')
+
+def get_all_files_in_dir(parent_path):
+    all_files = []
+    for path in os.listdir(parent_path):
+        path = os.path.join(parent_path, path)
+        if os.path.isfile(path):
+            all_files.append(path)
+        elif os.path.isdir(path):
+            all_files += (get_all_files_in_dir(path))
+    return all_files
 
 if __name__ == '__main__':
     app.run(debug=True)
