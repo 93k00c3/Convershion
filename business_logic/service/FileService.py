2006e11ee4ee7bf58d2b19209ba42dee4529fe7a
jruchel <jruchel254@gmail.com>
2023-01-28 17:47:31 +0100

    Added services for file upload and file conversion Added app.ini Added conversion.html Added convert_audio_files to FileConversionService

---
+++ business_logic/service/FileService.py
@@ -0,0 +1,49 @@
+import os
+from werkzeug.utils import secure_filename
+
+upload_path = 'C:\\Users\\admin\\Documents\\Convershion\\uploads'
+extensions = ['flac', 'alac', 'mp3', 'wav']
+
+
+def create_upload_folder_if_doesnt_exist(folder_name: str):
+    path = os.path.join(upload_path, folder_name)
+    try:
+        os.mkdir(path)
+    except Exception:
+        raise Exception('Folder \'{}\' already exists'.format(folder_name))
+
+
+def save_files(folder_name: str, files):
+    files_saved = 0
+    exception_messages = []
+    for key in files:
+        try:
+            save_file(folder_name, files[key][0])
+            files_saved += 1
+        except e:
+            exception_messages.append(str(e))
+    if exception_messages.size > 0:
+        raise Exception(join_errors(exception_messages))
+
+
+def join_errors(exception_messages: list):
+    message = ''
+    for error in exception_messages:
+        message += error + '\n'
+    return message
+
+
+def save_file(folder_name: str, file):
+    file_name = secure_filename(file.filename)
+    extension = file_name.split('.')[1]
+    if is_extension_allowed(extension) is False:
+        raise Exception("Extension: '{}' not allowed, supported extensions: {}".format(extension, extensions))
+    try:
+        create_upload_folder_if_doesnt_exist(folder_name)
+    except:
+        pass
+    file.save(os.path.join(upload_path, os.path.join(folder_name, file_name)))
+
+
+def is_extension_allowed(extension: str):
+    return extension in extensions
---
+++ test/FileServiceTest.py
@@ -0,0 +1,3 @@
+from business_logic.service.FileService import create_upload_folder_if_doesnt_exist
+
+create_upload_folder_if_doesnt_exist('test')
