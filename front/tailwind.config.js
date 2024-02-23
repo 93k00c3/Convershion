912767b1ec10a56f596308cd2a0d4ea555664301
93k00c3 <82811885+93k00c3@users.noreply.github.com>
2024-02-23 13:40:53 +0100

    Tailwind added to react

---
+++ front/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,tsx}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
