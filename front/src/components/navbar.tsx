6a275b4c0d5c50c1a41db7348a6e0d19d3f25782
93k00c3 <82811885+93k00c3@users.noreply.github.com>
2024-02-23 13:42:44 +0100

    Navbar moved and refactored

---
+++ front/src/components/navbar.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './navbar.css';
+import logoSvg from './logo.svg';
+import Login from './login.tsx';
+
+
+interface NavbarProps {
+  loggedIn: boolean;
+  username?: string;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ loggedIn, username }) => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleLoginClick = () => {
+    setShowLogin(true);
+};
+
+const handleLoginClose = () => {
+    setShowLogin(false);
+};
+  return (
+    <nav>
+      <div className="logo h-100 w-100">
+        <img src={logoSvg} alt="Logo" style={{ width: '50px', height: 'auto' }} />
+      </div>
+      <ul className="nav-links">
+        <li><a href="#">Convershion</a></li>
+      </ul>
+      <div className="login-register">
+        {loggedIn ? (
+          <>
+            <a href="#">Welcome, {username}</a>
+            <a href="/logout">Logout</a>
+          </>
+        ) : (
+          <>
+            <button onClick={handleLoginClick}>Login</button>
+            {showLogin && <Login onClose={handleLoginClose} />}             
+            <a href="/register">Register</a>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
