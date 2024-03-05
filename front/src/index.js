e58de1b19d5af5092ef139af2b2d83e2f0c62a94
93k00c3 <82811885+93k00c3@users.noreply.github.com>
2024-03-05 01:33:23 +0100

    AuthContextProvider in index

--- front/src/index.js
+++ front/src/index.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
+import { Navigate } from 'react-router-dom';
 import Navbar from './components/navbar.tsx';
 import Conversion from './components/conversion.tsx';
 import reportWebVitals from './reportWebVitals';
+import { AuthContext, AuthContextProvider } from './components/AuthContext.tsx'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+function ProtectedRoute({ children }) {
+  const { isLoggedIn } = useContext(AuthContext);
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
 root.render(
   <React.StrictMode>
-    <Router>
-      <Navbar />
-        <Routes>
-          <Route path="/" exact element={<App />} />
-          <Route path="/conversion" exact element={<Conversion />} />
-        </Routes>
-    </Router>
+    <AuthContextProvider>
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route path="/" exact element={<App />} />
+            <Route path="/conversion" exact  element={<ProtectedRoute><Conversion /></ProtectedRoute>} />
+          </Routes>
+      </Router>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
