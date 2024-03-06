50284013a0b74c480cb3fadd5f5e050d00a73cc0
93k00c3 <82811885+93k00c3@users.noreply.github.com>
2024-03-06 21:08:08 +0100

    Database changes

--- business_logic/service/DatabaseSetup.py
+++ business_logic/service/DatabaseSetup.py
@@ -8,10 +8,9 @@ class User(db.Model):
     __tablename__ = 'users'
 
     user_id = db.Column(db.Integer, primary_key=True)
-    user_uuid = db.Column(db.String(36), unique=True, nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(250), nullable=False)
     folder_name = db.Column(db.String(36), nullable=False)
 
     def set_password(self, password):
